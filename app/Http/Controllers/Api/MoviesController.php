b2bdaa0ec9d2e6babcfd1b668e73c2e0cab74b56
Lupi2602 <lupiranofan@gmail.com>
2025-04-24 13:06:51 +0700

    updatemovieterbaru

---
+++ app/Http/Controllers/Api/MoviesController.php
@@ -0,0 +1,28 @@
+<?php 
+ 
+namespace App\Http\Controllers\Api; 
+ 
+use App\Http\Controllers\Controller; 
+use App\Services\MovieApiService; 
+use Illuminate\Http\Request; 
+ 
+class MoviesController extends Controller 
+{ 
+    protected $apiService; 
+ 
+    // Menambahkan dependensi pada constructor
+    public function __construct(MovieApiService 
+    $apiService) 
+        { 
+            $this->apiService = $apiService; 
+        } 
+     
+        // Method untuk mendapatkan semua tim 
+        public function getMovies() 
+        { 
+            // Ambil data tim dari API 
+            $movies = $this->apiService->getMovies(); 
+            //return response()->json($movies); 
+             return view('movies', ['data' => $movies]);
+        } 
+    } 
